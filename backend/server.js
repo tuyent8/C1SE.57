912060d1a369dd95409cdd39e2a9565eae7c1e34
tuyent8 <minhtuyent8@gmail.com.>
2025-09-18 15:31:04 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Backend!'));
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
