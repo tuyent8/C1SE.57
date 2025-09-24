93b4eb895614711c2b8168e40806bcc25025b0df
tuyent8 <minhtuyent8@gmail.com.>
2025-09-24 18:01:28 +0700

    cap nhat code

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectSQL = require("./src/config/sql");
-const connectMongo = require("./src/config/mongo");
+
 
 const app = express();
 app.use(express.json());
 
 // Kết nối database
 connectSQL();
-connectMongo();
 
 app.get("/", (req, res) => {
   res.send("Backend connected to SQL Server + MongoDB 🚀");
